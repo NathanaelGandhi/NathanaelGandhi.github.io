c22f947df4ce82087b5443e19cefb6041c7a4054
Nathanael Gandhi <nat11public@gmail.com>
2023-03-15 22:45:23 +1100

    fix(util.js): sanitise input config

--- assets/js/util.js
+++ assets/js/util.js
@@ -95,8 +95,13 @@
 			}, userConfig);
 
 			// Expand "target" if it's not a jQuery object already.
-				if (typeof config.target != 'jQuery')
-					config.target = $(config.target);
+				if (!(config.target instanceof jQuery)) {
+					// Sanitize user-configured target element.
+					var sanitizedTarget = $('<div>').text(config.target).html();
+
+					// Convert sanitized target element to a jQuery object.
+					config.target = $(sanitizedTarget);					
+				}
 
 		// Panel.
 
